b8f767ac81ceaf6da0d0a0c64a8ef638b6d74096
Nikhil-MLOPs <nikhil.bhardwaj.mlops@gmail.com>
2025-08-19 19:14:16 +0530

    Project Structure is Ready.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An ML project for full hands-on experience with different MLOPs tools.',
+    author='Nikhil Bhardwaj',
+    license='MIT',
+)
